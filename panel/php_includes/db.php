2ee3fccf23c9c30febca80bbf95a7d80769ae4a0
ChrisStark <starkc918@gmail.com>
2021-01-29 16:09:10 -0500

    settings update

--- panel/php_includes/db.php
+++ panel/php_includes/db.php
@@ -1,16 +1,16 @@
 <?php
-define("WWWHOST", "police.arma3-life.com");
+/*define("WWWHOST", "cstark.me/panel");
 if($_SERVER["HTTP_HOST"] != WWWHOST) {
 	redirect("http://".WWWHOST."/".str_replace("/police/", "", $_SERVER["REQUEST_URI"]));
 	die();
-}
+}*/
 //error_reporting(E_ALL);
 ini_set('display_errors',0);
 ini_set('display_startup_errors',0);
 define('HOST', "localhost");
-define('USER', "arma3l_a3l");
-define('PASS', "f2SZVm0pej0MTfCvwZlp");
-define('DB', "arma3l_policedb");
+define('USER', "cstark_ncmausr");
+define('PASS', "%x_nmVJLHuk,");
+define('DB', "cstark_ncmapanel");
 define('BASEDIR', "/var/www/arma3l_police");
 
 function genPDO($DB = DB, $user = USER, $pass = PASS, $host = HOST) {
