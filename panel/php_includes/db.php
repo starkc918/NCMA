69194ee05f6be365c38dc340ddc9e5d694e1fd3f
ChrisStark <starkc918@gmail.com>
2021-01-29 16:56:46 -0500

    Update db.php

--- panel/php_includes/db.php
+++ panel/php_includes/db.php
@@ -4,14 +4,14 @@ if($_SERVER["HTTP_HOST"] != WWWHOST) {
 	redirect("http://".WWWHOST."/".str_replace("/police/", "", $_SERVER["REQUEST_URI"]));
 	die();
 }*/
-//error_reporting(E_ALL);
+error_reporting(E_ALL);
 ini_set('display_errors',0);
 ini_set('display_startup_errors',0);
 define('HOST', "localhost");
 define('USER', "cstark_ncmausr");
 define('PASS', "%x_nmVJLHuk,");
 define('DB', "cstark_ncmapanel");
-define('BASEDIR', "/var/www/arma3l_police");
+define('BASEDIR', "/var/www/cstark");
 
 function genPDO($DB = DB, $user = USER, $pass = PASS, $host = HOST) {
 	$pdo = new PDO("mysql:host=".$host.";dbname=".$DB, $user, $pass);
